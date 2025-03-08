f547e4ed52b9c3dfcc8264eee42f25498679cd09
ripXjoker <115973776+ripXjoker@users.noreply.github.com>
2025-03-08 07:17:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-css file
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+    alert('Thank you for contacting me! I will get back to you soon.');
+});
